12913d6b50c5c65662368b81bcc5ce07aca9f12f
Roely Chur <gerson.roely@gmail.com>
2022-06-09 11:33:01 -0600

    Indicador de Acciones (Dirección)

---
+++ app/Http/Controllers/Direccion/AccionesController.php
@@ -0,0 +1,201 @@
+<?php 
+
+namespace App\Http\Controllers\Direccion;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\DB;
+
+class AccionesController extends Controller{
+
+    public function create($indicador){
+
+        try {
+            
+            $data = (object) [
+                'date' => $indicador->date
+            ];
+
+            $result = (object) $this->data($data);
+
+            $chart = $this->chart($result);
+
+            $content = [
+                'chart' => $chart,
+            ];
+
+            $result->content = $content;
+                        
+            $indicador->data = $result;
+
+            return $indicador;
+
+        } catch (\Throwable $th) {
+            
+            $error = [
+                'message' => $th->getMessage()
+            ];
+
+            $indicador->error = $error;
+
+            return $indicador;
+
+        }
+        
+    }
+
+    public function chart($data){
+
+        $bottom_detail = $data->bottom_detail;
+
+        $chart = [
+            'type' => "Pie",
+            'chartData' => [
+                'labels' => [
+                   $bottom_detail[1]['text'],
+                   $bottom_detail[0]['text']
+                ],
+                'datasets'=> [
+                    [
+                        'data' => [
+                            $bottom_detail[1]['value'],
+                            $bottom_detail[0]['value']
+                        ],
+                        'backgroundColor' => [
+                            'rgb(54, 162, 235)',
+                            'rgba(255, 205, 86, 0)'
+                        ]
+                    ]
+                ],
+            ],
+            'chartOptions' => [
+                'responsive' => true,
+                'plugins' => [
+                    'legend' => [
+                        'display' => false,
+                    ],
+                ],
+            ],
+        ];
+
+        return $chart;
+
+    }
+
+    public function data($data){
+
+        $year = date('Y', strtotime($data->date));
+        $last_day_month = date('Y-m-t', strtotime($data->date));
+
+        $gestiones = DB::connection('rrhh')->select("   SELECT *
+                                                        FROM gc_gestion
+                                                        WHERE TO_CHAR(fecha_propuesta_cierre, 'YYYY') = '$year'
+                                                        AND ESTATUS_GESTION NOT IN ('PENDIENTE', 'ANULADO')
+                                                        AND FECHA_APERTURA_ACCION <= TO_DATE('$last_day_month', 'YYYY-MM-DD')");
+
+        $abiertas = [];
+        $cerradas = [];
+        $en_tiempo = 0;
+        $gestiones_en_tiempo = [];
+
+        foreach ($gestiones as $gestion) {  
+
+            // Contabilizar aquellas acciones que finalizaron en el tiempo estimado
+            if ((strtotime($gestion->fecha_real_cierre) <= strtotime($gestion->fecha_propuesta_cierre) && $gestion->estatus_gestion == 'FINALIZADO')) {
+               
+                $en_tiempo++;
+                $gestiones_en_tiempo [] = $gestion;
+
+            }
+
+            if ($gestion->estatus_gestion == 'FINALIZADO' && (strtotime($gestion->fecha_real_cierre) <= strtotime($last_day_month))) {
+                
+                $cerradas [] = $gestion;
+
+            }elseif ($gestion->estatus_gestion == 'EN PROCESO' || (strtotime($gestion->fecha_real_cierre) >= strtotime($last_day_month))) {
+                
+                $abiertas [] = $gestion;
+
+            }
+
+        }
+
+        $headers = [
+            [
+                'text' => 'ID',
+                'value' => 'id_gestion',
+                'sortable' => false
+            ],
+            [
+                'text' => 'Descripción',
+                'value' => 'descripcion',
+                'sortable' => false
+            ]
+        ];
+
+        $bottom_detail = [
+            [
+                'text' => 'Abiertas',
+                'value' => count($abiertas),
+                'detail' => [
+                    'table' => [
+                        'items' => $abiertas,
+                        'headers' => $headers
+                    ]
+                ],
+                'component' => 'tables/TableDetail'
+            ],
+            [
+                'text' => 'Cerradas',
+                'value' => count($cerradas),
+                'detail' => [
+                    'table' => [
+                        'items' => $cerradas,
+                        'headers' => $headers
+                    ]
+                ],
+                'component' => 'tables/TableDetail'
+            ],
+            [
+                'text' => 'Total',
+                'value' => count($gestiones),
+                'detail' => [
+                    'table' => [
+                        'items' => $gestiones,
+                        'headers' => $headers
+                    ]
+                ],
+                'component' => 'tables/TableDetail'
+            ]
+        ];
+
+        if (count($gestiones) > 0) {
+            
+            $avance = round((count($cerradas) / count($gestiones)) * 100, 1);
+            $eficacia = round(($en_tiempo / count($gestiones)) * 100, 1);
+
+        }
+
+        $response = [
+            'avance' => [
+                'text' => 'Avance',
+                'value' => $avance
+            ],
+            'eficacia' => [
+                'text' => 'Eficacia',
+                'value' => $eficacia,
+                'detail' => [
+                    'table' => [
+                        'items' => $gestiones_en_tiempo,
+                        'headers' => $headers
+                    ]
+                ],
+                'component' => 'tables/TableDetail'
+            ],
+            'bottom_detail' => $bottom_detail
+        ];
+
+        return $response;
+
+    }
+
+}
